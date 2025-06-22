06bdfb7b4e480bf0b864f0ed52f0993b79ecaefd
RAJVEER31 <88730146+RAJVEER31@users.noreply.github.com>
2025-06-22 10:09:14 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,3 +1,25 @@
+import java.util.ArrayList;
+
+class SingleTable {
+    private int number_of_seats;
+    private double desirability;
+    private int height;
+
+    public SingleTable(int number_of_seats, double desirability, int height) {
+        this.number_of_seats = number_of_seats;
+        this.desirability = desirability;
+        this.height = height;
+    }
+    public int getNumber_of_seats() {
+        return number_of_seats;
+    }
+    public double getDesirability() {
+        return desirability;
+    }
+    public int getHeight() {
+        return height;
+    }
+}
 class CombinedTable {
     private SingleTable table1;
     private SingleTable table2;
@@ -8,7 +30,7 @@ class CombinedTable {
     }
 
     public boolean canSeat(int numPeople) {
-        return (table1.getNumSeats() + table2.getNumSeats()) >= numPeople;
+        return (table1.getNumber_of_seats() + table2.getNumber_of_seats()) >= numPeople;
     }
 
     public double getDesirability() {
@@ -20,28 +42,31 @@ class CombinedTable {
             return avgDesirability - 10.0;
         }
     }
+    public int getNumber_of_seats(){
+        return table1.getNumber_of_seats() + table2.getNumber_of_seats();
+    }
 }
 
 class Table {
     private String name;
     private int height;
-    private ArrayList<CombinedTable> CombinedTables;
+    private ArrayList<CombinedTable> combinedTables;
 
-    public Table(String name, int id) {
+    public Table(String name, int height) {
         this.name = name;
         this.height = height;
-        CombinedTables = new ArrayList<>();
+        this.combinedTables = new ArrayList<>();
     }
 
-    public void addCombinedTable(CombinedTable CombinedTable) {
-        CombinedTables.add(CombinedTable);
+    public void addCombinedTables(CombinedTable combinedTables) {
+        combinedTables.add(combinedTables);
     }
 
-    public void removeCombinedTable(CombinedTable CombinedTable) {
-        CombinedTables.remove(CombinedTable);
+    public void removeCombinedTables(CombinedTables combinedTables) {
+        combinedTables.remove(combinedTables);
     }
-    public int getNumSeats(CombinedTable CombinedTable) {
-        return CombinedTable.getNumSeats();
+    public int getNumSeats(CombinedTables CombinedTables) {
+        return CombinedTables.getNumber_of_seats();
     }
     public int getHeight(int height){
         return height;
@@ -50,17 +75,29 @@ class Table {
         return viewQuality;
     }
     public void setViewQuality (double value){
-        this.value = value;
+        this.viewQuality = value;
     }
-
+    public String get_Transcript(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("Table: ").append(name).append("\n");
+        sb.append("Height: ").append(height).append("\n");
+        sb.append("View Quality: ").append(viewQuality).append("\n");
+        sb.append("Combined table").append(CombinedTables.size()).append("\n");
+        for (int i = 0; i < CombinedTables.size(); i++) {
+            CombinedTables ct = CombinedTables.get(i);
+            sb.append(" [").append(i + 1).append("] Seats: ").append(ct.getNumber_of_seats()).append(", Desirability: ").append(ct.getDesirability()).append("\n");
+        }
+        return sb.toString();
+    }
 }
-
 public class Main {
     public static void main(String[] args) {
-        Table table = new Table("c1", 4);
-        table.addCombinedTable(new CombinedTable("Math", 3));
-        table.addCombinedTable(new CombinedTable("Science", 4));
-        System.out.println(table.getTranscript());
-        
+        SingleTable t1 = new SingleTable(4, 8.5, 5);
+        SingleTable t2 = new SingleTable(2, 9.5, 4);
+        CombinedTables ct1 = new CombinedTables(t1, t2);
+        Table table = new Table("Table 1", 450);
+        table.setViewQuality(9.5);
+        table.addCombinedTables(ct1);
+        System.out.println(table.get_Transcript());
     }
 }
